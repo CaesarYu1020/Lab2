241c86b407786cd8febef8f81bb6f2a8e6e1e74b
CaesarYu1020 <bluesea861020@gmail.com>
2018-10-04 14:45:58 +0800

    Lab3

--- app/src/main/java/com/example/user/lab2/MainActivity.java
+++ app/src/main/java/com/example/user/lab2/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int rnum,mnum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,73 +27,86 @@ public class MainActivity extends AppCompatActivity {
         final TextView me=findViewById(R.id.me);
         final TextView ai=findViewById(R.id.ai);
         final RadioGroup rg=findViewById(R.id.rg);
-
+        final TextView show=findViewById(R.id.show);
+        mnum=-1;
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                name.setText(et.getText());
 
-                rnum=(int)(Math.random()*3)+1;
-
-                switch (rnum){
-                    case 1:
-                        ai.setText("剪刀");
-                        break;
-                    case 2:
-                        ai.setText("石頭");
-                        break;
-                    case 3:
-                        ai.setText("布");
-                        break;
-                }
-
-
-                switch(rg.getCheckedRadioButtonId()){
-                    case R.id.rb1:
-                        me.setText("剪刀");
-                        mnum=1;
-                        break;
-                    case R.id.rb2:
-                        me.setText("石頭");
-                        mnum=2;
-                        break;
-                    case R.id.rb3:
-                        me.setText("布");
-                        mnum=3;
-                        break;
-                }
-                if(rnum==1){
-                    if(mnum==1)
-                        win.setText("平手");
-
-                    else if(mnum==2)
-                        win.setText("玩家");
-
-                    else
-                         win.setText("電腦");
+                if(et.getText().toString().equals(""))
+                {show.setText("請選擇玩家名稱");}
+                else {
+                    rnum = (int) (Math.random() * 3) + 1;
+                    name.setText(et.getText());
+                    switch (rnum) {
+                        case 1:
+                            ai.setText("剪刀");
+                            break;
+                        case 2:
+                            ai.setText("石頭");
+                            break;
+                        case 3:
+                            ai.setText("布");
+                            break;
                     }
-                if(rnum==2){
-                    if(mnum==2)
-                        win.setText("平手");
 
-                    else if(mnum==3)
-                        win.setText("玩家");
+                    if (mnum == -1) {
+                        show.setText("請選擇出拳的種類");
+                    }else{
+                    switch (rg.getCheckedRadioButtonId()) {
+                        case R.id.rb1:
+                            me.setText("剪刀");
+                            mnum = 1;
+                            break;
+                        case R.id.rb2:
+                            me.setText("石頭");
+                            mnum = 2;
+                            break;
+                        case R.id.rb3:
+                            me.setText("布");
+                            mnum = 3;
+                            break;
+                    }
 
-                    else
-                        win.setText("電腦");
+                    if (rnum == 1) {
+                        if (mnum == 1)
+                            win.setText("平手");
+
+                        else if (mnum == 2) {
+                            win.setText(name.getText().toString());
+                            show.setText("恭喜你獲勝了!!");
+                        } else {
+                            win.setText("電腦");
+                            show.setText("可惜，電腦獲勝了");
+                        }
+                    }
+                    if (rnum == 2) {
+                        if (mnum == 2)
+                            win.setText("平手");
+
+                        else if (mnum == 3) {
+                            win.setText(name.getText().toString());
+                            show.setText("恭喜你獲勝了!!");
+                        } else {
+                            win.setText("電腦");
+                            show.setText("可惜，電腦獲勝了");
+                        }
+                    }
+                    if (rnum == 3) {
+                        if (mnum == 3)
+                            win.setText("平手");
+
+                        else if (mnum == 1) {
+                            win.setText(name.getText().toString());
+                            show.setText("恭喜你獲勝了!!");
+                        } else {
+                            win.setText("電腦");
+                            show.setText("可惜，電腦獲勝了");
+                        }
+                    }
                 }
-                if(rnum==3){
-                    if(mnum==3)
-                        win.setText("平手");
-
-                    else if(mnum==1)
-                        win.setText("玩家");
-
-                    else
-                        win.setText("電腦");
                 }
-
 
 
             }
